a6fc01f503ad20cafd3909344d102ed3ae5622a8
cyberwichi <cyberwichi@gmail.com>
2019-06-04 04:28:32 +0200

    ultimas rutas

--- app/Http/Controllers/RepationsController.php
+++ app/Http/Controllers/RepationsController.php
@@ -10,7 +10,7 @@ class RepationsController extends Controller
 {
     public function getAll()
     {
-        $reparations = Reparations::all();
+        $reparations = Reparations::orderBy('matricula')->get();
         return $reparations;
     }
     public function add(Request $request)
@@ -25,30 +25,75 @@ class RepationsController extends Controller
     }
     public function getAllForFecha($fecha)
     {
-        $reparations = Reparations::where('fecha', $fecha)->get();
+        $reparations = Reparations::where('fecha', $fecha)
+        ->orderBy('matricula')
+        ->get();
         return $reparations;
     }
     public function getAllForDesrepara($desrepara)
     {
         $reparations = Reparations::query()
             ->where('desrepara', 'LIKE', "%{$desrepara}%")
+            ->orderBy('matricula')
             ->get();
         return $reparations;
     }
-    public function getAllForMatriculaAndFecha($matricula, $fecha){
-        $reparations=Reparations::query()
+    public function getAllForMatriculaAndFecha($matricula, $fecha)
+    {
+        $reparations = Reparations::query()
             ->where('matricula', $matricula)
             ->where('fecha', $fecha)
+            ->orderBy('matricula')
             ->get();
         return $reparations;
     }
-    public function editById($id, Request $request){
-        $reparacion=Reparations::find($id);
+    public function getAllForMatriculaFechaAndDesrepara($matricula, $fecha, $desrepara)
+    {
+        $reparations = Reparations::query()
+            ->where('matricula', $matricula)
+            ->where('fecha', $fecha)
+            ->where('desrepara', 'LIKE', "%{$desrepara}%")
+            ->orderBy('matricula')
+            ->get();
+        return $reparations;
+    }
+    public function getAllForMatriculaAndDesrepara($matricula, $desrepara)
+    {
+        $reparations = Reparations::query()
+            ->where('matricula', $matricula)
+            ->where('desrepara', 'LIKE', "%{$desrepara}%")
+            ->ordertBy('matricula')
+            ->get();
+        return $reparations;
+    }
+    public function getAllForFechaAndDesrepara($fecha, $desrepara)
+    {
+        $reparations = Reparations::query()
+            ->where('fecha', $fecha)
+            ->where('desrepara', 'LIKE', "%{$desrepara}%")
+            ->orderBy('matricula')
+            ->get();
+        return $reparations;
+    }
+    public function getAllForMatriculaAndFechaAndDesrepara($matricula, $fecha, $desrepara){
+        $reparations = Reparations::query()
+            ->where('matricula',$matricula)
+            ->where('fecha', $fecha)
+            ->where('desrepara', 'LIKE', "%{$desrepara}%")
+            ->orderBy('matricula')
+            ->get();
+        return $reparations;
+
+    }
+    public function editById($id, Request $request)
+    {
+        $reparacion = Reparations::find($id);
         $reparacion->fill($request->all())->save();
         return $reparacion;
     }
-    public function deleteById($id){
-        $reparacion=Reparations::find($id);
+    public function deleteById($id)
+    {
+        $reparacion = Reparations::find($id);
         $reparacion->delete();
         return $reparacion;
     }
--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,12 @@ Route::group(['middleware' => 'cors'], function()
     Route::get('reparaciones/fecha/{fecha}', 'RepationsController@getAllForFecha')->name('getAllForOneFecha');
     Route::get('reparaciones/desrepara/{concepto}', 'RepationsController@getAllForDesrepara')->name('getAllForOneDesrepara');
     Route::get('reparaciones/{matricula}/fecha/{fecha}', 'RepationsController@getAllForMatriculaAndFecha')->name('getAllForMatriculaAndFecha');
+    Route::get('reparaciones/{matricula}/desrepara/{desrepara}', 'RepationsController@getAllForMatriculaAndDesrepara')->name('getAllForMatriculaAndDesrepara');
+    Route::get('reparaciones/all/{matricula}/{fecha}/{desrepara}', 'RepationsController@getAllForMatriculaFechaAndDesrepara')->name('getAllForMatriculaFechaAndDesrepara');
+    Route::get('reparaciones/fecha/{fecha}/desrepara/{desrepara}', 'RepationsController@getAllForFechaAndDesrepara')->name('getAllForFechaAndDesrepara');
+    Route::get('reparaciones/{matricula}/fecha/{fecha}/desrepara/{desrepara}', 'RepationsController@getAllForMatriculaAndFechaAndDesrepara')->name('getAllForMatriculaAndFechaAndDesrepara');
     Route::post('reparaciones/{id}', 'RepationsController@editById')->name('editReparationsById');
     Route::get('reparaciones/delete/{id}', 'RepationsController@deleteById')->name('deleteReparationsById');
+
 });
 
